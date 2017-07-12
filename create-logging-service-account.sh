d6ceb232b3319550e66575c4801556abc91faf6c
John LaBarge <johnlabarge@google.com>
2017-07-12 16:26:56 -0700

    shell script to create logging service account

---
+++ create-logging-service-account.sh
@@ -0,0 +1,12 @@
+#!/bin/sh 
+PROJECT=$(gcloud config get-value project)
+
+gcloud iam service-accounts create logging-service-account --display-name "logging-service-account"
+
+gcloud projects add-iam-policy-binding $PROJECT \
+  --member serviceAccount:logging-service-account@${PROJECT}.iam.gserviceaccount.com \
+  --role roles/logging.logWriter
+
+gcloud iam service-accounts keys create \
+  logging-service-account.json \
+  --iam-account logging-service-account@${PROJECT}.iam.gserviceaccount.com
