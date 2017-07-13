66df3dda4fde480a2eac1e1b91d98ca7e779701e
John LaBarge <johnlabarge@google.com>
2017-07-13 15:25:39 -0700

    finish logging daemonset update

--- create-logging-service-account.sh
+++ create-logging-service-account.sh
@@ -7,6 +7,10 @@ gcloud projects add-iam-policy-binding $PROJECT \
   --member serviceAccount:logging-service-account@${PROJECT}.iam.gserviceaccount.com \
   --role roles/logging.logWriter
 
+gcloud projects add-iam-policy-binding $PROJECT \
+  --member serviceAccount:logging-service-account@${PROJECT}.iam.gserviceaccount.com \
+  --role roles/editor
+
 gcloud iam service-accounts keys create \
   logging-service-account.json \
   --iam-account logging-service-account@${PROJECT}.iam.gserviceaccount.com
